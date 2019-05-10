8780e8177c6419193c5ef859aa5e586821ead6ac
quangnuce <35477020+quangnuce@users.noreply.github.com>
2019-05-10 11:38:00 +0700

    Add files via upload

---
+++ test/motorcontroller.ino
@@ -0,0 +1,116 @@
+#include "ros.h"
+
+#include "geometry_msgs/Twist.h"
+
+#define STEP1 5
+#define DIR1 4
+#define STEP2 7
+#define DIR2 6
+float x;
+long i;
+long step_count;
+long delayrotate;
+float rotate;
+int level;
+ros::NodeHandle nh;
+int dir;//0: forward,-1:left, 1 right
+void velCallback(  const geometry_msgs::Twist& vel)
+{
+  x = vel.linear.x; // I CAN USE VEL AS I WANT
+ 
+  rotate = vel.angular.z;
+  if (rotate < 0) {
+    dir = -1;
+  }
+  if (rotate > 0) {
+    dir = 1;
+  }
+  if (rotate == 0)
+    dir = 0;
+  float absHR = rotate < 0 ? -rotate : rotate;
+  if (0 < absHR && absHR <= PI / 6)
+    level = 1;
+  else if (PI / 6 < absHR && absHR <= PI / 3)
+    level = 2;
+  else if (absHR == 0) level = 0;
+  else level = 3;
+  if(x==0&&dir!=0)
+    x=0.2;
+     step_count = x / 0.000471;
+  delayrotate = 1000000 / step_count;
+  
+  
+  //in log
+  char log_msg[50];
+  char str_temp[6];
+  dtostrf(x, 4, 2, str_temp);
+  sprintf(log_msg,"x=%s,level=%d,dir=%d",str_temp,level,dir);
+  nh.loginfo(log_msg);
+
+}
+
+ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel" , velCallback);
+
+void setup() {
+  Serial.begin(9600);
+  nh.initNode();
+  nh.subscribe(sub);
+  x = 0;
+  rotate = 0;
+  step_count = 0;
+
+  pinMode(STEP1, OUTPUT); // Step
+  pinMode(DIR1, OUTPUT); // Dir - chiều quay
+  pinMode(STEP2, OUTPUT); // Step
+  pinMode(DIR2, OUTPUT); // Dir - chiều quay
+}
+
+void loop() {
+  nh.spinOnce();
+
+    
+  digitalWrite(DIR1, LOW); // Set Dir high  - Set chiều quay mức cao
+  digitalWrite(DIR2, HIGH); // Dir - chiều quay
+  if (dir == 0) {
+    for (i = 0; i < step_count; i++) 
+    {
+      digitalWrite(STEP1, HIGH); // Output high - xung mức cao
+      digitalWrite(STEP2, HIGH); // Output high - xung mức cao
+      delayMicroseconds(delayrotate);         // Trễ 1ms
+      digitalWrite(STEP1, LOW); // Output low - xung mức thấp
+      digitalWrite(STEP2, LOW); // Output low - xung mức thấp
+      delayMicroseconds(delayrotate);         // Trễ 3ms
+    }
+  }
+  else if (dir > 0) {
+    for (i = 0; i < step_count; i++) 
+    {
+      if (i % 3 <= level)
+        digitalWrite(STEP1, HIGH); // Output high - xung mức cao
+      digitalWrite(STEP2, HIGH); // Output high - xung mức cao
+      delayMicroseconds(delayrotate);
+      // Trễ 1ms
+      if (i % 3 <= level)
+        digitalWrite(STEP1, LOW); // Output low - xung mức thấp
+      digitalWrite(STEP2, LOW); // Output low - xung mức thấp
+      delayMicroseconds(delayrotate);         // Trễ 3ms
+    }
+  }
+  else {
+    for (i = 0; i < step_count; i++) 
+    {
+
+      digitalWrite(STEP1, HIGH); // Output high - xung mức cao
+      if (i % 3 <= level)
+        digitalWrite(STEP2, HIGH); // Output high - xung mức cao
+      delayMicroseconds(delayrotate);
+      // Trễ 1ms
+
+      digitalWrite(STEP1, LOW); // Output low - xung mức thấp
+      if (i % 3 <= level)
+        digitalWrite(STEP2, LOW); // Output low - xung mức thấp
+      delayMicroseconds(delayrotate);         // Trễ 3ms
+    }
+  }
+
+}
---
+++ test/motorcontroller2.ino
@@ -0,0 +1,104 @@
+#include "ros.h"
+#include "geometry_msgs/Twist.h"
+#define STEP1 5
+#define DIR1 4
+#define STEP2 7
+#define DIR2 6
+float x;
+float z;
+long step1;
+long kc;
+ros::NodeHandle nh;
+void velCallback(  const geometry_msgs::Twist& vel_cmd)
+{
+  x = vel_cmd.linear.x;
+  z = vel_cmd.angular.z;
+  if(x==0&&z!=0)
+    x=0.2;
+}
+ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel" , velCallback);
+void setup()
+
+{
+  nh.initNode();
+  nh.subscribe(sub);
+  pinMode(STEP1, OUTPUT); // Step
+  pinMode(DIR1, OUTPUT); // Dir - chiều quay
+  pinMode(STEP2, OUTPUT); // Step
+  pinMode(DIR2, OUTPUT); // Dir - chiều quay
+
+}
+void tinh (float x)
+{
+  step1 = x / 0.00047124;
+  kc = 1000000 / step1;//microsecond
+  kc=kc/2;
+  //in log
+  char log_msg[50];
+  char str_x[6];
+  dtostrf(x, 4, 2, str_x);
+  sprintf(log_msg,"x=%s,step1=%ld, kc=%ld",str_x,step1,kc);
+  nh.loginfo(log_msg);
+}
+long buoc (float x, float z)
+{
+  float abstan=tan(z)>0?tan(z):-tan(z);
+  long stepp = (x - abstan * 0.15) / 0.00047124;
+  long tyle = step1 / stepp;
+  char log_msg[50];
+  char str_x[6];
+  dtostrf(abstan, 4, 2, str_x);
+  sprintf(log_msg,"tang=%s,tyle=%ld,stepp=%ld,step1=%ld",str_x,tyle,stepp,step1);
+  nh.loginfo(log_msg);
+  if(tyle<0) tyle=2147483647;
+  return tyle;
+}
+void loop()
+{
+  nh.spinOnce();
+  tinh (x);
+  Serial.println(step1);
+  Serial.print(kc);
+  digitalWrite(DIR1, LOW); // Set Dir high  - Set chiều quay mức cao
+  digitalWrite(DIR2, HIGH); // Dir - chiều quay
+  long tyle=buoc(x, z);
+  char log_msg[50];
+  char str_x[6];
+  dtostrf(z, 4, 2, str_x);
+  sprintf(log_msg,"tyle=%ld,z=%s,kc=%ld ",tyle,str_x,kc);
+  nh.loginfo(log_msg);
+  //tiến thẳng
+  if (z == 0)
+    for (long i = 0; i < step1; i++)
+    {
+      digitalWrite(STEP1, HIGH); // Output high - xung mức cao
+      digitalWrite(STEP2, HIGH); // Output high - xung mức cao
+      delayMicroseconds(kc);
+      digitalWrite(STEP1, LOW); // Output low - xung mức thấp
+      digitalWrite(STEP2, LOW); // Output low - xung mức thấp
+      delayMicroseconds(kc );       // Trễ kc ms
+    }
+  else if ( z > 0)
+    // trái:phải(re trai)
+    for (long i = 1; i <= step1; i++)
+
+    { if (i % tyle == 0)  digitalWrite(STEP1, HIGH); // Output high - xung mức cao
+      digitalWrite(STEP2, HIGH); // Output high - xung mức cao
+      delayMicroseconds(kc );
+      if (i % tyle == 0)  digitalWrite(STEP1, LOW); // Output low - xung mức thấp
+      digitalWrite(STEP2, LOW); // Output low - xung mức thấp
+      delayMicroseconds(kc );
+    }
+  else
+    //rẽ phải
+    for (long i = 1; i <= step1; i++)
+
+    { if (i % tyle == 0)  digitalWrite(STEP2, HIGH); // Output high - xung mức cao
+      digitalWrite(STEP1, HIGH); // Output high - xung mức cao
+      delayMicroseconds(kc);
+      if (i % tyle == 0)  digitalWrite(STEP2, LOW); // Output low - xung mức thấp
+      digitalWrite(STEP1, LOW); // Output low - xung mức thấp
+      delayMicroseconds(kc);
+    }
+
+}
